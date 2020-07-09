d91ef28f10a102ced0ae799386f1093b6c4f8e31
Carlos <c.hdezbueno@gmail.com>
2020-07-09 21:47:33 +0200

    asd

--- codigoFuenteTFG.ino
+++ codigoFuenteTFG.ino
@@ -68,48 +68,48 @@ int option;
 
 void setup()
 {
-  // Definimos el puerto serie
-  Serial.begin(9600);
+    // Definimos el puerto serie
+    Serial.begin(9600);
 
-  /*
+    /*
  * PIR sensor
  */
-  pinMode(ledPin, OUTPUT);  // Declaramos el pin del led como salida
-  pinMode(inputPin, INPUT); // Declaramos el pin del sensor como entrada
-  pinMode(8, OUTPUT);
-  /*
+    pinMode(ledPin, OUTPUT);  // Declaramos el pin del led como salida
+    pinMode(inputPin, INPUT); // Declaramos el pin del sensor como entrada
+    pinMode(8, OUTPUT);
+    /*
  * DHT sensor
  */
 
-  /*
+    /*
  * Servo motor
  */
-  //myservo.attach(SERVO_MOTOR_PIN); // Une el servo en el pin al objeto servo
+    //myservo.attach(SERVO_MOTOR_PIN); // Une el servo en el pin al objeto servo
 
-  /*
+    /*
  * Relés
  */
-  /* pinMode(rele_termostato, OUTPUT);
+    /* pinMode(rele_termostato, OUTPUT);
   pinMode(rele_luz1, OUTPUT);
   pinMode(rele_luz2, OUTPUT);
   pinMode(rele_persiana1, OUTPUT);
   pinMode(rele_persiana2, OUTPUT); */
 
-  /*
+    /*
  * Fotoresistor
  */
-  int calibracionFotoresistor;
+    int calibracionFotoresistor;
 
-  calibracionFotoresistor = analogRead(pResistor);
+    calibracionFotoresistor = analogRead(pResistor);
 
-  if (calibracionFotoresistor > 50)
-  {
-    hayLuz = "true";
-  }
-  else if (calibracionFotoresistor <= 50)
-  {
-    hayLuz = "false";
-  }
+    if (calibracionFotoresistor > 50)
+    {
+        hayLuz = "true";
+    }
+    else if (calibracionFotoresistor <= 50)
+    {
+        hayLuz = "false";
+    }
 }
 
 /*
@@ -117,123 +117,123 @@ void setup()
  */
 static bool measure_environment(float *temperature, float *humidity)
 {
-  static unsigned long measurement_timestamp = millis();
+    static unsigned long measurement_timestamp = millis();
 
-  /* Mide cada segundo. */
-  if (millis() - measurement_timestamp > 1000ul)
-  {
-    if (dht_sensor.measure(temperature, humidity) == true)
+    /* Mide cada segundo. */
+    if (millis() - measurement_timestamp > 1000ul)
     {
+        if (dht_sensor.measure(temperature, humidity) == true)
+        {
 
-      measurement_timestamp = millis();
-      return (true);
+            measurement_timestamp = millis();
+            return (true);
+        }
     }
-  }
 
-  return (false);
+    return (false);
 }
 
 void loop()
 {
 
-  // Primero leemos los datos de los sensores y los escribimos en el puerto serie
+    // Primero leemos los datos de los sensores y los escribimos en el puerto serie
 
-  /*
- * PIR sensor
- */
+    /*
+  * PIR sensor
+  */
 
-  val = digitalRead(inputPin); // Lee el input
-  if (val == HIGH)
-  {
-    // Si el input esta en HIGH significa que hemos detectado movimiento
-
-    digitalWrite(ledPin, HIGH); // Encendemos el led
-                                // Se considera que ha habido movimiento
-    pirState = "true";
-  }
-  else
-  {
-    // Si el input esta en LOW significa que NO hemos detectado movimiento
-    digitalWrite(ledPin, LOW); // Apagamos el led
-    // Se considera que no ha habido movimiento
-    pirState = "false";
-  }
-
-  /*
- * Fotoresistor
- */
-
-  valorFotoresistor = analogRead(pResistor); //Con el valor lo tratamos despues en el servicio que recibe los datos.
-
-  if (valorFotoresistor > 50)
-    hayLuz = "true";
-
-  else if (valorFotoresistor < 40)
-    hayLuz = "false";
-
-  //Posteriormente leemos los datos que hay en el puerto serie para leer las ordenes que lleguen
-
-  if (Serial.available() > 0)
-  {
-
-    //leemos la opcion enviada
-
-    //option = Serial.read();
-
-    String data;
-    data = Serial.readString();
-
-    Serial.println(data);
-    Serial.flush();
-
-    if (data.equals("enviame"))
+    val = digitalRead(inputPin); // Lee el input
+    if (val == HIGH)
     {
-      unsigned long tiempoinicial, tiempoIntermedio;
-      tiempoinicial = tiempoIntermedio = millis();
+        // Si el input esta en HIGH significa que hemos detectado movimiento
 
-      while (tiempoIntermedio - tiempoinicial < 10000ul)
-      {
-        /*
-        * Sensor DHT
-        */
-
-        bool medision = measure_environment(&temperatura, &humedad);
-
-        if (medision)
-        {
-          temperaturaTemp = temperatura;
-          humedadTemp = humedad;
-          break;
-        }
-        tiempoIntermedio = millis();
-      }
-
-      String json = "{\"temperatura\":" + String(temperatura) + ", \"humedad\":" + String(humedad) + ", \"luz\":" + hayLuz + ", \"movimiento\":" + pirState + "}";
-      Serial.println(json);
-      Serial.flush();
-    }
-    else if (data == "v")
-    {
-      //Conectar servomotor
-      digitalWrite(8, HIGH);
-      Serial.println("OK");
-    }
-    else if (data.toInt() != 0)
-    {
-
-      long pin = data.toInt();
-
-      if (pin <= 10)
-        digitalWrite(8, HIGH);
-      if (pin > 10)
-        digitalWrite(8, LOW);
-      //Conectar Reles
-
-      Serial.println("OK");
+        digitalWrite(ledPin, HIGH); // Encendemos el led
+                                    // Se considera que ha habido movimiento
+        pirState = "true";
     }
     else
-    { //Ninguna orden coincide
-      Serial.println("No");
+    {
+        // Si el input esta en LOW significa que NO hemos detectado movimiento
+        digitalWrite(ledPin, LOW); // Apagamos el led
+        // Se considera que no ha habido movimiento
+        pirState = "false";
     }
-  }
+
+    /*
+  * Fotoresistor
+  */
+
+    valorFotoresistor = analogRead(pResistor); //Con el valor lo tratamos despues en el servicio que recibe los datos.
+
+    if (valorFotoresistor > 50)
+        hayLuz = "true";
+
+    else if (valorFotoresistor < 40)
+        hayLuz = "false";
+
+    //Posteriormente leemos los datos que hay en el puerto serie para leer las ordenes que lleguen
+
+    if (Serial.available() > 0)
+    {
+
+        //leemos la opcion enviada
+
+        //option = Serial.read();
+
+        String data;
+        data = Serial.readString();
+
+        Serial.println(data);
+        Serial.flush();
+
+        if (data.equals("enviame"))
+        {
+            unsigned long tiempoinicial, tiempoIntermedio;
+            tiempoinicial = tiempoIntermedio = millis();
+
+            while (tiempoIntermedio - tiempoinicial < 10000ul)
+            {
+                /*
+          * Sensor DHT
+          */
+
+                bool medision = measure_environment(&temperatura, &humedad);
+
+                if (medision)
+                {
+                    temperaturaTemp = temperatura;
+                    humedadTemp = humedad;
+                    break;
+                }
+                tiempoIntermedio = millis();
+            }
+
+            String json = "{\"temperatura\":" + String(temperatura) + ", \"humedad\":" + String(humedad) + ", \"luz\":" + hayLuz + ", \"movimiento\":" + pirState + "}";
+            Serial.println(json);
+            Serial.flush();
+        }
+        else if (data == "v")
+        {
+            //Conectar servomotor
+            digitalWrite(8, HIGH);
+            Serial.println("OK");
+        }
+        else if (data.toInt() != 0)
+        {
+
+            long pin = data.toInt();
+
+            if (pin <= 10)
+                digitalWrite(8, HIGH);
+            if (pin > 10)
+                digitalWrite(8, LOW);
+            //Conectar Reles
+
+            Serial.println("OK");
+        }
+        else
+        { //Ninguna orden coincide
+            Serial.println("No");
+        }
+    }
 }
